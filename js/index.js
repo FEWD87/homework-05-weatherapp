599d89365ce2ca489e74d42fc1cc56ad06f3b8cb
crystalxlau <crystal.lau11@gmail.com>
2016-07-12 11:17:04 -0400

    hw submit

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(function(){
 
   $("#submit").click(convertTemp);
 
-  function convertTemp() {
+  function convertTemp(event) {
     event.preventDefault();
 
     $("#tempF").html("&#8457"); 
@@ -11,21 +11,23 @@ $(function(){
     var fahrenheit = enteredTemp * 1.8 + 32;
 
     $("#tempF").prepend(fahrenheit.toFixed(1));
-//     if (fahrenheit <= 30 && fahrenheit >= -30) {
-//       $("body").attr("class", "freezing");
 
-//     } else if (fahrenheit > 30 && fahrenheit <= 60) {
-//       $("body").attr("class", "cold");
+    if (fahrenheit < 30 & fahrenheit >= -30) {
+      // console.log('read temperature')
+      $("body").attr("class", "freezing");
 
-//     } else if (fahrenheit > 60 && fahrenheit <= 75) {
-//       $("body").attr("class", "warm");
+    } else if (fahrenheit > 30 & fahrenheit <= 60) {
+      $("body").attr("class", "cold");
 
-//     } else if (fahrenheit > 75 && fahrenheit <= 95) {
-//       $("body").attr("class", "hot");
+    } else if (fahrenheit > 60 & fahrenheit <= 75) {
+      $("body").attr("class", "warm");
 
-//     } else (fahrenheit > 95) {
-//       $("body").attr("class", "boiling");
-//     }
+    } else if (fahrenheit > 75 & fahrenheit <= 95) {
+      $("body").attr("class", "hot");
+
+    } else if (fahrenheit > 95 & fahrenheit <= 300) {
+      $("body").attr("class", "burning");
+    }
 
   }
 
